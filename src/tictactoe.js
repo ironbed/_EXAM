2c2b97bdb2bdf0825f6bf6aea2b2aa6faa69c908
Gyula Szabó <gyula.szabo2@ibm.com>
2023-04-28 08:27:27 +0200

    UAT-1.5 green

--- src/tictactoe.js
+++ src/tictactoe.js
@@ -2,10 +2,12 @@ class TicTacToe {
   board;
   actualPlayer;
   topMessage;
+  bottomMessage;
   constructor() {
     this.board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']];
     this.actualPlayer = 'X';
     this.topMessage = 'Game Board Creation...';
+    this.bottomMessage = 'Board Created.\nThe game will start with player X';
   }
 }
 
